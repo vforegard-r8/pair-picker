8dc88b589a2e9e86de55be69c504f56fe61320e6
vforegard-r8 <vforegard@rise8.us>
2026-06-02 16:43:28 -0400

    Fix: Configure Vite to build to 'build' folder instead of 'dist'

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build'
+  },
   server: {
     port: 3000,
     proxy: {
